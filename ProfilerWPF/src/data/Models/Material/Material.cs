87b943ca17b50c8d9df7984871c63a1391e71c7b
Ashima74 <105321051+Ashima74@users.noreply.github.com>
2022-05-11 08:48:40 +1000

    Add files via upload

---
+++ ProfilerWPF/src/data/Models/Material/Material.cs
@@ -0,0 +1,11 @@
+﻿namespace ProfilerWPF.data.Models.Material
+{
+    class Material
+    {
+        int number { get; set; }
+        string name { get;  set; }
+
+        public Material() { }
+    }
+    
+}
