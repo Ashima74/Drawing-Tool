c93b8be741d1fcd691e3e9de9ab20fdcee7a6981
Ashima74 <105321051+Ashima74@users.noreply.github.com>
2022-05-11 08:53:58 +1000

    Add files via upload

---
+++ ProfilerWPF/src/data/SqlLiteSettings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Configuration;
+
+namespace ProfilerWPF.data
+{
+    class SqlLiteSettings : ISqlLiteSettings
+    {
+        public string GetConnectionString()
+        {
+            var databaseFilePath = AppDomain.CurrentDomain.BaseDirectory + "../../../../RobotSystems XS2515 - SQLite format 3.cfg";
+            string fixedConnectionString = ConfigurationManager.ConnectionStrings["default"].ConnectionString.Replace("{DbPath}", databaseFilePath);
+            return fixedConnectionString;
+        }
+    }
+
+    public interface ISqlLiteSettings
+    {
+        string GetConnectionString( );
+    }
+}
