95b70fc1eb25cc7b0a81fda577b7aefb074322a5
Ashima74 <105321051+Ashima74@users.noreply.github.com>
2022-05-11 10:01:59 +1000

    Add files via upload

---
+++ ProfilerWPF/src/gui/DataTypes/Interfaces/ContextMenuItem.cs
@@ -0,0 +1,48 @@
+﻿using Prism.Commands;
+
+namespace ProfilerWPF.gui.DataTypes.Interfaces
+{
+    //This class contains a text element and a command, used for specifying behaiviour for context menu items
+    public class ContextMenuItem
+    {
+        public ContextMenuItem(string text, string subText = null, System.Windows.Style icon = null, DelegateCommand command = null)
+        {
+            Text = text;
+            Icon = icon;
+            SubText = subText;
+            Command = command;
+        }
+
+        private DelegateCommand CommandProperty;
+
+        public DelegateCommand Command
+        {
+            get { return CommandProperty; }
+            set { CommandProperty = value; }
+        }
+
+        private object IconProperty;
+
+        public object Icon
+        {
+            get { return IconProperty; }
+            set { IconProperty = value; }
+        }
+
+
+        private string SubTextProperty;
+        public string SubText
+        {
+            get { return SubTextProperty; }
+            set { SubTextProperty = value; }
+        }
+
+
+        private string TextProperty;
+        public string Text
+        {
+            get { return TextProperty; }
+            set { TextProperty = value; }
+        }
+    }
+}
